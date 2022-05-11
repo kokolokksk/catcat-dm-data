b64ff18896258d2548414d6e6510d9fede91b80a
loveloliii <woshigegefengleng@live.com>
2022-05-11 18:01:02 +0800

    hide param

--- src/main/kotlin/monster/loli/catcatdmdata/controller/WebLoginController.kt
+++ src/main/kotlin/monster/loli/catcatdmdata/controller/WebLoginController.kt
@@ -2,17 +2,15 @@ package monster.loli.catcatdmdata.controller
 
 import monster.loli.catcatdmdata.service.WebLoginService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("web")
 class WebLoginController {
     @Autowired
     lateinit var webLoginService: WebLoginService
-    @RequestMapping("login/{clientId}")
-    fun loginWeb(@PathVariable("clientId") clientId:String ): LinkedHashMap<String, Any> {
+    @PostMapping("login")
+    fun loginWeb(@RequestBody clientId:String ): LinkedHashMap<String, Any> {
         var dataMap = LinkedHashMap<String,Any>()
         var rs = webLoginService.loginWeb(clientId);
         if(rs.size==0){
