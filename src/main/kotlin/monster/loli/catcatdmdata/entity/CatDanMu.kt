f7d9f515b5d8e349b5deb77fd82e867ebf9b2f78
sayari <loli@loli.monster>
2022-06-14 14:57:11 +0800

    fix data struct

--- src/main/kotlin/monster/loli/catcatdmdata/entity/CatDanMu.kt
+++ src/main/kotlin/monster/loli/catcatdmdata/entity/CatDanMu.kt
@@ -7,18 +7,18 @@ import org.springframework.core.annotation.AliasFor
 import org.springframework.data.mongodb.core.mapping.Field
 
 data class CatDanMu(
-    var userid:String = "",
-    var uuid: Long? = null,
+    var uuid: String? = null,
+    var type:Int = 0,
+    var uid: Long? = null,
     var nickname:String="",
-    var avatar:String ="",
-    var live_level:String ="",
-    var xz_Level:String = "",
-    var xz_name:String = "",
-    var danmu:String = "",
-    var time:Long = 0,
-    var use_state:Long = 0,
-    var type:Int = 1,
-    var clientId:String = "",
+    var content:String = "",
+    var price:Long =0,
+    var giftName:String ="",
+    var giftImg:String = "",
+    var timestamp:String = "",
+    var giftType:Int = 0,
+    var avatarFace:String = "",
     var roomId:Long? = null,
-    var sessionId:String = ""
+    var sessionId:String = "",
+    var clientId:String = ""
 )
